fd32504f54f0ca4b91fd8150fbb2edf2c3e90c64
Guillaume Kotulski <guillaume.kotulski@4d.com>
2024-08-01 12:33:14 +0200

    add new configurations

--- editor/src/extension.ts
+++ editor/src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 
-
+const kPortNumber : number = 19815;
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "4d-debug" is now active!');
-	activateMockDebug(context);
+	start(context);
 }
 
 // This method is called when your extension is deactivated
@@ -13,41 +13,37 @@ export function deactivate() {
 }
 
 
-export function activateMockDebug(context: vscode.ExtensionContext) {
+export function start(context: vscode.ExtensionContext) {
 
-	const provider = new MockConfigurationProvider();
+	const provider = new ConfigurationProvider();
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('4d', provider));
 
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('4d', {
 		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+			console.log('provideDebugConfigurations', folder);
 			return [
 				{
-					name: "Dynamic Launch",
-					request: "launch",
+					name: "4D:Attach",
+					request: "attach",
 					type: "4d",
-					program: "${file}"
+					program: "${workspaceFolder}",
+					port: kPortNumber,
 				},
 				{
-					name: "Another Dynamic Launch",
+					name: "4D:Launch",
 					request: "launch",
 					type: "4d",
-					program: "${file}"
-				},
-				{
-					name: "4D Launch",
-					request: "launch",
-					type: "4d",
-					program: "${file}"
+					program: "${workspaceFolder}"
 				}
 			];
 		}
 	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
-	let factory: vscode.DebugAdapterDescriptorFactory = new MockDebugAdapterServerDescriptorFactory();
+	let factory: vscode.DebugAdapterDescriptorFactory = new DebugAdapterServerDescriptorFactory();
 	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('4d', factory));
 
 }
 
-class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
+class ConfigurationProvider implements vscode.DebugConfigurationProvider {
 
 	/**
 	 * Message a debug configuration just before a debug session is being launched,
@@ -56,13 +52,15 @@ class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
 	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
 
 		// if launch.json is missing or empty
+		console.log(config);
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === 'markdown') {
+			if (editor && editor.document.languageId === '4d') {
 				config.type = '4d';
-				config.name = 'Launch';
-				config.request = 'launch';
-				config.program = '${file}';
+				config.name = '4D:Attach';
+				config.request = 'attach';
+				config.port = kPortNumber;
+				config.program = '${workspaceFolder}';
 				config.stopOnEntry = true;
 			}
 		}
@@ -75,15 +73,14 @@ class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
 
 		return config;
 	}
+
 }
 
 
-
-
-class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+class DebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
 	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-		let port = session?.configuration.port ?? 19815;
+		let port = session?.configuration.port ?? kPortNumber;
 		// make VS Code connect to debug server
 		return new vscode.DebugAdapterServer(port);
 	}
